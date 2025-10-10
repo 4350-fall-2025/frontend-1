2224553847496edb6fbd41947ceddb36791dc97d
arasantos <100260167+arasantos@users.noreply.github.com>
2025-10-09 20:23:31 -0500

    feat(QDOG-54): Create login page (#7)
    Add login page to the app.tsx so it shows in path /
    Add design tokens and keep global styles in globals.scss
    Add fonts in layout.tsx
    Add responsive style for login page in page.module.scss, mostly matching figma
    Add images for pet owner and vet, can be temporary.
    Remove unused styles in globals.scss
    Add validations for email and password
    Change config to allow allowImportingTsExtensions so we can import custom functions that we made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client" // doesn't use server
+
+import Image, { StaticImageData } from "next/image";
+import { Tabs, TabsList, TabsTab, TextInput, PasswordInput, Button } from "@mantine/core";
+import { useState } from "react";
+import { useForm } from '@mantine/form';
+import { validateEmail, validatePassword } from "../util/validation/validation.ts";
+import styles from "./page.module.scss";
+import ownerImage from "../../public/login/petOwner.jpg" // source: https://unsplash.com/photos/woman-hugging-a-dog-FtuJIuBbUhI
+import vetImage from "../../public/login/vet.jpg" // source: https://www.freepik.com/free-photo/close-up-doctor-checking-cat-s-belly_23442502.htm#fromView=keyword&page=1&position=32&uuid=d7e73635-ac35-41b6-80b1-b544a20a5f68&query=Vet
 
 export default function Home() {
-    return (
-        <div className={styles.page}>
-            <main className={styles.main}>
-                <Image
-                    className={styles.logo}
-                    src='/next.svg'
-                    alt='Next.js logo'
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol>
-                    <li>
-                        Get started by editing <code>src/app/page.tsx</code>.
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
+  const owner: string = "owner";
+  const vet: string = "vet";
 
-                <div className={styles.ctas}>
-                    <a
-                        className={styles.primary}
-                        href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <Image
-                            className={styles.logo}
-                            src='/vercel.svg'
-                            alt='Vercel logomark'
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className={styles.secondary}
-                    >
-                        Read our docs
-                    </a>
-                </div>
-            </main>
-            <footer className={styles.footer}>
-                <a
-                    href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    <Image
-                        aria-hidden
-                        src='/file.svg'
-                        alt='File icon'
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    <Image
-                        aria-hidden
-                        src='/window.svg'
-                        alt='Window icon'
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    <Image
-                        aria-hidden
-                        src='/globe.svg'
-                        alt='Globe icon'
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: { email: "", password: "" },
+
+    validate: {
+      email: validateEmail,
+      password: validatePassword
+    },
+  });
+
+  // sets selected tab/user to be owner by default
+  const [selectedUser, setSelectedUser] = useState(owner);
+
+  const emailPlaceholder: string = "youremail@email.com";
+
+  const backdropImage: StaticImageData = selectedUser == owner ? ownerImage : vetImage;
+  const backdropImageAltText: string = selectedUser == owner ? "Dog with the pet owner" : "Cat with vets";
+
+  const buttonColor: string = selectedUser == owner ? styles.button_owner : styles.button_vet;
+
+  return (
+    <div className={styles.page}>
+      <main>
+        <div className={styles.grid}>
+          <div className={styles.login_section}>
+            <h1> Welcome to QDog!</h1>
+
+            <Tabs value={selectedUser} onChange={setSelectedUser}>
+              <TabsList>
+                <TabsTab value={owner}>Pet Owner</TabsTab>
+                <TabsTab value={vet}>Veterinarian</TabsTab>
+              </TabsList>
+            </Tabs>
+
+            <form className={`${styles.tabs_panel} ${selectedUser}`} onSubmit={form.onSubmit(console.log)}>
+              <TextInput
+                label="Email"
+                placeholder={emailPlaceholder}
+                key={form.key("email")}
+                {...form.getInputProps("email")}
+              />
+
+              <PasswordInput
+                label="Password"
+                key={form.key("password")}
+                {...form.getInputProps("password")}
+              />
+
+              <Button type="submit" className={buttonColor} variant="filled">Login</Button>
+            </form>
+
+            <div className={styles.login_footer}>
+              <a href="">Forgot password</a>
+              <p>Don't have an account yet? <a href="">Sign up!</a></p>
+            </div>
+          </div>
+
+          <div className={styles.image_grid}>
+            <Image src={backdropImage} alt={backdropImageAltText}></Image>
+          </div>
         </div>
-    );
+      </main >
+    </div >
+  );
 }
