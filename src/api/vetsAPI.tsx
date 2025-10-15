863d52a7489ed0287be097f7e7530ffab5dfca16
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-10-15 13:44:03 -0500

    uncommented out code for vet api

--- src/api/vetsAPI.tsx
+++ src/api/vetsAPI.tsx
@@ -1,7 +1,6 @@
 import axiosClient from "./axiosClient";
 import { Vet } from "src/models/vet";
 
-//Note: currently all functions returns mock info, with the actual api code is commented out
 export class VetsAPI {
     //filters out null values from the owner object
     //ChatGPT-5 was used to make this function
@@ -12,30 +11,22 @@ export class VetsAPI {
     }
 
     static async getVet(id: number): Promise<Vet> {
-        // const response = await axiosClient.get(`/vets/${id}`);
-        // return response?.data;
-        return new Vet({
-            email: "test@gmail.com",
-            firstName: "Test",
-            lastName: "Account",
-            password: "12345678",
-            id: 1,
-            certification: "yes",
-        });
+        const response = await axiosClient.get(`/vets/${id}`);
+        return new Vet(response?.data);
     }
 
     //input: updated parameters for pets, with unchanged fields as null
     static async updateVet(id: number, vet: Vet): Promise<void> {
-        // const changedValues = this.removeNull(vet);
-        // const response = await axiosClient.put(`/vets/${id}`, changedValues);
+        const changedValues = this.removeNull(vet);
+        await axiosClient.put(`/vets/${id}`, changedValues);
     }
 
     static async deleteVet(id: number): Promise<void> {
-        // const response = await axiosClient.delete(`/vets/${id}`);
+        await axiosClient.delete(`/vets/${id}`);
     }
 
     static async vetSignUp(vet: Vet): Promise<void> {
-        const response = await axiosClient.post(`/vets/signup`, vet);
+        await axiosClient.post(`/vets/signup`, vet);
     }
 
     static async vetLogin(loginJSON): Promise<Vet> {
