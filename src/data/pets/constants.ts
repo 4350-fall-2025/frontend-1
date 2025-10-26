ba3aa926bd809fef84bcdd87024625b341d613cd
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-10-26 17:48:11 -0500

    created enum for animal group and modifiied constant to follow the label,value format

--- src/data/pets/constants.ts
+++ src/data/pets/constants.ts
@@ -1,13 +1,43 @@
-export const animalGroupOptions: string[] = [
-    "Small mammal (e.g. cat, rabbit, and mouse)",
-    "Farm (e.g. chicken, pig, and cow)",
-    "Equine (e.g. horse)",
-    "Bird",
-    "Reptile",
-    "Amphibian",
-    "Fish",
-    "Invertebrate",
-    "Other",
+import { AnimalGroup } from "src/models/pet";
+
+export const animalGroupOptions = [
+    {
+        value: AnimalGroup.small,
+        label: "Small mammal (e.g. cat, rabbit, and mouse)",
+    },
+    {
+        value: AnimalGroup.farm,
+        label: "Farm (e.g. chicken, pig, and cow)",
+    },
+    {
+        value: AnimalGroup.equine,
+        label: "Equine (e.g. horse)",
+    },
+    {
+        value: AnimalGroup.bird,
+        label: "Bird",
+    },
+    {
+        value: AnimalGroup.reptile,
+        label: "Reptile",
+    },
+    {
+        value: AnimalGroup.amphibian,
+        label: "Amphibian",
+    },
+
+    {
+        value: AnimalGroup.fish,
+        label: "Fish",
+    },
+    {
+        value: AnimalGroup.invertebrate,
+        label: "Invertebrate",
+    },
+    {
+        value: AnimalGroup.other,
+        label: "Other",
+    },
 ];
 
 export const sexOptions: string[] = ["Male", "Female", "Unknown"];
