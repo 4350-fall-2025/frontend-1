f90cd9627ddbe9e8f7b0f773873fc8ca04a4a94f
arasantos <100260167+arasantos@users.noreply.github.com>
2025-10-31 23:16:29 -0500

    Chore: Write unit and UI tests for pet diary
    Write unit and UI tests for pet diary
    Modify mock.ts to use a name that doesn't include number
    Add constants for min and max chars for diary, so it can be reused and easily maintained
    Add mock local storage and put in setupTests so other test files can use this
    Create new diary form helper to help with pet diary tests
    Write unit tests for validation of pet diary content

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -3,3 +3,7 @@ export const defaultDate: Date = new Date(0);
 export const todayDate: Date = new Date();
 
 export const allowedImageTypes = ["image/jpeg", "image/png", "image/webp"];
+
+// can be reused for vet notes and pet diary
+export const notesMinCharacters = 2;
+export const notesMaxCharacters = 1500;
