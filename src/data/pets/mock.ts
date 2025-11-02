b5d033b497262f265773c116525addceb83dfc12
arasantos <100260167+arasantos@users.noreply.github.com>
2025-11-02 13:25:06 -0600

    Remove unused variable in mock.ts

--- src/data/pets/mock.ts
+++ src/data/pets/mock.ts
@@ -1,13 +1,10 @@
 import { AnimalGroup, Pet, SterileStatus } from "src/models/pet";
-import { todayDate } from "~data/constants";
-
-const todayDateString: string = todayDate.toISOString();
 
 export const mockPets: Pet[] = [
     {
         id: "pet1",
         name: "Bella",
-        birthdate: todayDateString,
+        birthdate: new Date("2019-08-01").toISOString(),
         estimatedBirthdate: true,
         sex: "Female",
         animalGroup: AnimalGroup.amphibian,
@@ -19,7 +16,7 @@ export const mockPets: Pet[] = [
     {
         id: "pet2",
         name: "Tweety",
-        birthdate: todayDateString,
+        birthdate: new Date("2024-03-01").toISOString(),
         estimatedBirthdate: true,
         sex: "Female",
         animalGroup: AnimalGroup.bird,
