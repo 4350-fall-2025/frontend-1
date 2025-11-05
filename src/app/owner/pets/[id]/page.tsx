a643a359b497d7666f07464956c7396beee308b7
JordonH03 <jordonhong03@gmail.com>
2025-11-05 15:01:29 -0600

    chore: Update styles

--- src/app/owner/pets/[id]/page.tsx
+++ src/app/owner/pets/[id]/page.tsx
@@ -70,93 +70,99 @@ export default function PetProfilePage() {
                     Back to My Pets
                 </Button>
             </div>
-            <h1 className={styles.header}>{pet?.name}</h1>
-            <div className={styles.pet_info}>
-                <Image
-                    className={styles.pet_image}
-                    src='/placeholder.jpg'
-                    width={200}
-                    height={200}
-                    alt='Pet profile picture'
-                />
-                <div className={styles.pet_details}>
-                    <div className={styles.pet_details_column}>
-                        <ul>
-                            <li>
-                                <span className={styles.pet_info_label}>
-                                    Sex:
-                                </span>{" "}
-                                {pet?.sex && toSentenceCase(pet?.sex)}
-                            </li>
-                            <li>
-                                <span className={styles.pet_info_label}>
-                                    Animal group:
-                                </span>{" "}
-                                {pet?.animalGroup &&
-                                    formatAnimalGroup(pet?.animalGroup)}
-                            </li>
-                            <li>
-                                <span className={styles.pet_info_label}>
-                                    Species:
-                                </span>{" "}
-                                {pet?.species && toSentenceCase(pet?.species)}
-                            </li>
-                            <li>
-                                <span className={styles.pet_info_label}>
-                                    Breed:
-                                </span>{" "}
-                                {pet?.breed && toSentenceCase(pet?.breed)}
-                            </li>
-                        </ul>
-                    </div>
-                    <div className={styles.pet_details_column}>
-                        <ul>
-                            <li>
-                                <span className={styles.pet_info_label}>
-                                    Birthdate:
-                                </span>{" "}
-                                {pet?.birthdate &&
-                                    dayjs(pet?.birthdate).format(
-                                        "MMMM D, YYYY",
-                                    )}
-                            </li>
-                            <li>
-                                <span className={styles.pet_info_label}>
-                                    Age:{" "}
-                                </span>
-                                {pet?.birthdate &&
-                                    formatAgeFromDOB(pet?.birthdate)}
-                            </li>
-                            <li>
-                                <span className={styles.pet_info_label}>
-                                    Spayed/Neutered:
-                                </span>{" "}
-                                {pet?.sterileStatus &&
-                                    formatSterileStatus(
-                                        pet?.sterileStatus,
-                                        pet?.sex,
-                                    )}
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.pet_notes}>
-                <div className={styles.pet_notes_column}>
-                    <h2>Diary Entries</h2>
-                    <div className={styles.diary_entries_list}>
-                        {diaryEntries.length > 0 ? (
-                            diaryEntries.map((entry) => (
-                                <DiaryEntry key={entry.id + 1} entry={entry} />
-                            ))
-                        ) : (
-                            <p>No entries yet.</p>
-                        )}
+            <div className={styles.page_content}>
+                <h1 className={styles.header}>{pet?.name}</h1>
+                <div className={styles.pet_info}>
+                    <Image
+                        className={styles.pet_image}
+                        src='/placeholder.jpg'
+                        width={200}
+                        height={200}
+                        alt='Pet profile picture'
+                    />
+                    <div className={styles.pet_details}>
+                        <div className={styles.pet_details_column}>
+                            <ul>
+                                <li>
+                                    <span className={styles.pet_info_label}>
+                                        Sex:
+                                    </span>{" "}
+                                    {pet?.sex && toSentenceCase(pet?.sex)}
+                                </li>
+                                <li>
+                                    <span className={styles.pet_info_label}>
+                                        Animal group:
+                                    </span>{" "}
+                                    {pet?.animalGroup &&
+                                        formatAnimalGroup(pet?.animalGroup)}
+                                </li>
+                                <li>
+                                    <span className={styles.pet_info_label}>
+                                        Species:
+                                    </span>{" "}
+                                    {pet?.species &&
+                                        toSentenceCase(pet?.species)}
+                                </li>
+                                <li>
+                                    <span className={styles.pet_info_label}>
+                                        Breed:
+                                    </span>{" "}
+                                    {pet?.breed && toSentenceCase(pet?.breed)}
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={styles.pet_details_column}>
+                            <ul>
+                                <li>
+                                    <span className={styles.pet_info_label}>
+                                        Birthdate:
+                                    </span>{" "}
+                                    {pet?.birthdate &&
+                                        dayjs(pet?.birthdate).format(
+                                            "MMMM D, YYYY",
+                                        )}
+                                </li>
+                                <li>
+                                    <span className={styles.pet_info_label}>
+                                        Age:{" "}
+                                    </span>
+                                    {pet?.birthdate &&
+                                        formatAgeFromDOB(pet?.birthdate)}
+                                </li>
+                                <li>
+                                    <span className={styles.pet_info_label}>
+                                        Spayed/Neutered:
+                                    </span>{" "}
+                                    {pet?.sterileStatus &&
+                                        formatSterileStatus(
+                                            pet?.sterileStatus,
+                                            pet?.sex,
+                                        )}
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.pet_notes_column}>
-                    <h2>Vet Notes</h2>
-                    <p>No notes to show.</p>
+                <div className={styles.pet_notes}>
+                    <div className={styles.pet_notes_column}>
+                        <h2>Diary Entries</h2>
+                        <div className={styles.diary_entries_list}>
+                            {diaryEntries.length > 0 ? (
+                                diaryEntries.map((entry) => (
+                                    <DiaryEntry
+                                        key={entry.id + 1}
+                                        entry={entry}
+                                    />
+                                ))
+                            ) : (
+                                <p>No entries yet.</p>
+                            )}
+                        </div>
+                    </div>
+                    <div className={styles.pet_notes_column}>
+                        <h2>Vet Notes</h2>
+                        <p>No notes to show.</p>
+                    </div>
                 </div>
             </div>
         </div>
