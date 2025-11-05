77ae614af141ddead8deddfca65e2527390cfdea
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2025-11-04 22:26:24 -0600

    feat: Add pet diary entries to pet profile page

---
+++ src/components/diaryEntry/diaryEntry.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import dayjs from "dayjs";
+import { useRouter } from "next/navigation";
+
+import { toSentenceCase } from "~util/strings/normalize";
+import { PetDiary } from "src/models/pet-diary";
+import styles from "./diaryEntry.module.scss";
+
+export default function DiaryEntry({ entry }: { entry: PetDiary }) {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/under-construction`);
+    };
+
+    return (
+        <div className={styles.entry_card} onClick={handleClick}>
+            <div className={styles.entry_header}>
+                <h3 className={styles.heading}>
+                    {toSentenceCase(entry.contentType)}
+                </h3>
+                <span>
+                    {dayjs(entry.createTimestamp).format("MMMM D, YYYY")}
+                </span>
+            </div>
+            <p className={styles.entry_body}>{entry.contentBody}</p>
+        </div>
+    );
+}
