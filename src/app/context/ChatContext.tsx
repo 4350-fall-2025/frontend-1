62dc9998990a460e79c8425b17bb7feeb0ee9760
basmaabdulrazaq <abdulrab@myumanitoba.ca>
2025-12-02 17:23:50 -0600

    remove unused import

--- src/app/context/ChatContext.tsx
+++ src/app/context/ChatContext.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { createContext, useContext, useEffect, useState, useRef } from "react";
-import { Vet } from "src/models/vet";
 import {
     getAuthenticatedOwner,
     getAuthenticatedVet,
