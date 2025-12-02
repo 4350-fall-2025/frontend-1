1fec0c9d804ada2427795d08b36fdd7eb8ecc3a4
BasmaAbdulRazaq <123525422+BasmaAbdulRazaq@users.noreply.github.com>
2025-12-02 09:41:34 -0600

    created vet's websocket context for chat and requests page (#64)
    * created vet's websocket context for chat and requests page
    
    * moved announcing online to be in the vet related requests page to make the context reusable
    
    * feature: finalized websocket context and chat with vet and updated chat component to handle messaging
    
    * resolving sonarQuebe warnings
    
    * moved vet requests page to be under messages instead for consistency
    
    * removed unused import
    
    * reordered accept and reject buttons
    
    ---------
    
    Co-authored-by: basmaabdulrazaq <abdulrab@myumanitoba.ca>

---
+++ src/app/context/ChatContext.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState, useRef } from "react";
+import { Vet } from "src/models/vet";
+import { getAuthenticatedVet } from "~util/auth/getAuthenticatedUser";
+import { Client } from "@stomp/stompjs";
+import { generateWebSocketUrl } from "~data/messages/constants";
+
+const ChatContext = createContext(null);
+
+export const ChatProvider = ({ children }) => {
+    const [websocket, setWebsocket] = useState<Client>(null);
+    const currentPartner = useRef(null);
+    const petID = useRef(null);
+
+    useEffect(() => {
+        if (!websocket) {
+            let vet: Vet = getAuthenticatedVet();
+            const connection = new Client({
+                brokerURL: generateWebSocketUrl(vet.id),
+                onConnect: () => {
+                    console.log("connected :D ");
+                    setWebsocket(connection);
+                },
+            });
+
+            connection.onStompError = function (frame) {
+                console.log(
+                    "Broker reported error: " + frame.headers["message"],
+                );
+                console.log("Additional details: " + frame.body);
+            };
+            connection.activate();
+            return () => {
+                connection.deactivate();
+            };
+        }
+        return () => {
+            websocket.deactivate();
+        };
+    }, []);
+
+    return (
+        <ChatContext.Provider
+            value={{
+                websocket,
+                currentPartner,
+                petID,
+            }}
+        >
+            {children}
+        </ChatContext.Provider>
+    );
+};
+
+export const useSocket = () => useContext(ChatContext);
